46634863187f8bf42fa91bfeda7509bd27fe1af5
ak47andrew <ak47reborntumpa@gmail.com>
2025-05-28 19:02:24 +0300

    GameObject (no testing yet)

--- engine/gameobject.py
+++ engine/gameobject.py
@@ -1,4 +1,44 @@
+from typing import Any
 from .component import Component
+from typing import overload
+from .utils import PriorityList
 
 class GameObject:
-    pass
+    components: dict[str, Component]
+    components_to_run: PriorityList[Component]
+    storage: dict[str, dict[str, Any]]
+
+    @overload
+    def get_storage(self, component: str) -> dict[str, Any]:
+        ...
+
+    @overload
+    def get_storage(self, component: Component):
+        ...
+
+    def get_storage(self, component: str | Component) -> dict[str, Any]:
+        if isinstance(component, Component):
+            return self.get_storage(component.name)
+        return self.storage.get(component, {})
+
+    def add_component(self, component: Component) -> None:
+        if component.name in self.components:
+            raise ValueError(f"Component '{component.name}' already exists")
+        self.components[component.name] = component
+        self.storage[component.name] = component.get_default_storage()
+
+    @overload
+    def remove_component(self, component: str) -> None:
+        ...
+
+    @overload
+    def remove_component(self, component: Component) -> None:
+        ...
+
+    def remove_component(self, component: str | Component) -> None:
+        if isinstance(component, Component):
+            self.remove_component(component.name)
+        if component not in self.components:
+            raise ValueError(f"Component '{component}' does not exist")
+        del self.storage[component]
+        del self.components[component]
---
+++ engine/utils.py
@@ -0,0 +1,34 @@
+class PriorityList[T]:
+    data: list[tuple[T, int]]
+    # --- Magic methods ----
+    def __init__(self):
+        self.data = []
+
+    def __iter__(self):
+        return iter(self.data)
+
+    # --- Public methods ---
+    def add(self, item: T, priority: int) -> None:
+        index = self._binary_search((item, priority))
+        self.data.insert(index, (item, priority))
+
+    def get(self, item: T) -> tuple[T, int] | None:
+        for i, (x, priority) in enumerate(self.data):
+            if x == item:
+                return (x, priority)
+        return None
+
+    def remove(self, item: T) -> None:
+        self.data.remove(self.get(item))
+
+    # ---- Util methods ----
+
+    def _binary_search(self, item: tuple[T, int]) -> int:
+        low, high = 0, len(self.data)
+        while low < high:
+            mid = (low + high) // 2
+            if self.data[mid][1] < item[1]:  # Compare the integer part
+                low = mid + 1
+            else:
+                high = mid
+        return low
